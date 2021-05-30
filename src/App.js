a61789a0bff48f9da79712c2cc6d0d4dcdba652e
Shivam <shivamsinghl498@gmail.com>
2021-05-30 15:32:50 +0530

    Weather app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="App">
+        <main>
+            <div className="search-box">
+             <input type="text" className="search-bar" placeholder="Search..."/>
+            </div>
+        </main>
+        </div>
+    );
+}
+export default App;
